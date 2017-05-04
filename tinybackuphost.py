152f30b33dc385e895db83aa25605d8d5aed36f3
maxhaxuk <c01dtwitt@gmail.com>
2017-05-04 15:23:31 +0100

    Update tinybackuphost.py

--- tinybackuphost.py
+++ tinybackuphost.py
@@ -12,10 +12,10 @@ backup_file = backup_file.split('@')
 for i in range(len(backup_file)):
 	backup_file[i] = backup_file[i].split('!')
 for i in backup_file:
-	print(i[0])
 	directory = "backup"+'/'.join(i[0].split('/')[:-1])
-	print(directory)
-	if not os.path.exists(directory):
-		os.makedirs(directory)
-	with open("backup"+'/'.join(i[0].split('/')), 'w') as writefile:
-		writefile.write(b64decode(i[1]))
+	if directory!="backup":
+		print("Extracting: backup"+i[0])
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+		with open("backup"+i[0], 'w') as writefile:
+			writefile.write(b64decode(i[1]))
